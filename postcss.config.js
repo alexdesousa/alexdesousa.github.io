f2a555a95f9f080a3a282d670a4c02dc9d55f014
Alexander de Sousa <alexander.jose.de.sousa@gmail.com>
2020-03-29 19:46:19 +0200

    Attempt to generate the minimized CSS file

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,13 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        './cv.html',
+        './_layouts/cv.html',
+        './_includes/cv/*.html',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ]
 }
